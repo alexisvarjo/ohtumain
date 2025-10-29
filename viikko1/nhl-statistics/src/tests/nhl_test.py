34ebc55a4a443e01c7e854f5314662fcfc81a8a4
Alexis <alexisvarj@gmail.com>
2025-10-29 23:53:51 +0200

    teht 16

---
+++ viikko1/nhl-statistics/src/tests/nhl_test.py
@@ -0,0 +1,56 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+from unittest.mock import patch
+from io import BytesIO
+from player_reader import PlayerReader
+
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),  #  4+12 = 16
+            Player("Lemieux", "PIT", 45, 54),  # 45+54 = 99
+            Player("Kurri", "EDM", 37, 53),  # 37+53 = 90
+            Player("Yzerman", "DET", 42, 56),  # 42+56 = 98
+            Player("Gretzky", "EDM", 35, 89),  # 35+89 = 124
+        ]
+
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(PlayerReaderStub())
+
+    def test_nonexistent_player(self):
+        res = self.stats.search("Selänne")
+        self.assertIsNone(res)
+
+    def test_existing_player(self):
+        res = self.stats.search("Kurri")
+        exp = Player("Kurri", "EDM", 37, 53)
+        self.assertEqual(res.name, exp.name)
+        self.assertEqual(res.team, exp.team)
+        self.assertEqual(res.goals, exp.goals)
+        self.assertEqual(res.assists, exp.assists)
+
+    def test_team(self):
+        res = self.stats.team("PIT")
+        exp = [Player("Lemieux", "PIT", 45, 54)]
+        self.assertEqual(exp[0].name, res[0].name)
+        self.assertEqual(res[0].team, exp[0].team)
+        self.assertEqual(res[0].goals, exp[0].goals)
+        self.assertEqual(res[0].assists, exp[0].assists)
+
+    def test_top(self):
+        res = self.stats.top(1)
+        exp = [Player("Gretzky", "EDM", 35, 89)]
+        self.assertEqual(exp[0].name, res[0].name)
+        self.assertEqual(res[0].team, exp[0].team)
+        self.assertEqual(res[0].goals, exp[0].goals)
+        self.assertEqual(res[0].assists, exp[0].assists)
+
+    def test_player(self):
+        res = Player("Semenko", "EDM", 4, 12).__str__()
+        exp = "Semenko EDM 4 + 12 = 16"
+        self.assertEqual(exp, res)
