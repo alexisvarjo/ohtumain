fe8176f8f0088dff93294dc207cd2da5956190bb
Alexis <alexisvarj@gmail.com>
2025-11-08 15:23:27 +0200

    vika

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,8 +1,12 @@
+from erotus import erotus
 from logger import logger
+from summa import summa
 
 logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
+print(f"{summa(x, y)}")
+print(f"{erotus(x, y)}")
 
 logger("lopetetaan")
