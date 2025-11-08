ce9c7bc0f53bb59a17a3536735e2eca675274ebb
Alexis <alexisvarj@gmail.com>
2025-11-08 13:36:25 +0200

    tein vahingossa tehtävät 1-5 väärään repoon, ohtuvarasto repoon. nyt viikko 2 kansio näyttää miltä pitäisi t. 5 jälkeen

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
     def __init__(self, dict):
-        self.name = dict['name']
-    
+        self.name = dict["name"]
+        self.nationality = dict["nationality"]
+        self.assists = dict["assists"]
+        self.goals = dict["goals"]
+        self.points = self.assists + self.goals
+        self.team = dict["team"]
+        self.games = dict["games"]
+
     def __str__(self):
-        return self.name
+        return f"{self.name:20} {self.team:15} {self.goals:2} + {self.assists:2} = {self.points:2}"
